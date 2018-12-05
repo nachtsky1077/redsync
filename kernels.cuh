9983f821f4386fe13ab7f5a26d61535949849deb
Yangzihao Wang <yzhwang@ucdavis.edu>
2018-12-05 19:47:55 +0800

    Finished all kernel primitives.

--- kernels.cuh
+++ kernels.cuh
@@ -37,6 +37,19 @@ struct LargerThan
     }
 };
 
+// Functor type for selecting values less than some threshold
+struct LargerThanKV
+{
+    float compare;
+    __host__ __device__ __forceinline__
+    LargerThanKV(float compare) : compare(compare) {}
+
+    __host__ __device__ __forceinline__
+    bool operator()(const float2 &a) const {
+        return (a.y > compare);
+    }
+};
+
 void MeanKernel(cub::CachingDeviceAllocator &g_allocator, float *in, float *out, int num_items) {
     AbsSum sum_op;
     float init = 0.0f;
@@ -47,12 +60,13 @@ void MeanKernel(cub::CachingDeviceAllocator &g_allocator, float *in, float *out,
     CubDebugExit(cub::DeviceReduce::Reduce(d_temp_storage, temp_storage_bytes, in, out, num_items, sum_op, init));
     CubDebugExit(g_allocator.DeviceAllocate(&d_temp_storage, temp_storage_bytes));
     CubDebugExit(cub::DeviceReduce::Reduce(d_temp_storage, temp_storage_bytes, in, out, num_items, sum_op, init));
+    if (d_temp_storage) CubDebugExit(g_allocator.DeviceFree(d_temp_storage));
 
 }
 
 void MaxKernel(cub::CachingDeviceAllocator &g_allocator, float *in, float *out, int num_items) {
     AbsMax max_op;
-    float init = FLT_MIN;
+    float init = 0.0f;
 
     void *d_temp_storage = NULL;
     size_t temp_storage_bytes = 0;
@@ -60,6 +74,7 @@ void MaxKernel(cub::CachingDeviceAllocator &g_allocator, float *in, float *out, 
     CubDebugExit(cub::DeviceReduce::Reduce(d_temp_storage, temp_storage_bytes, in, out, num_items, max_op, init));
     CubDebugExit(g_allocator.DeviceAllocate(&d_temp_storage, temp_storage_bytes));
     CubDebugExit(cub::DeviceReduce::Reduce(d_temp_storage, temp_storage_bytes, in, out, num_items, max_op, init));
+    if (d_temp_storage) CubDebugExit(g_allocator.DeviceFree(d_temp_storage));
 }
 
 void CountNonZero(cub::CachingDeviceAllocator &g_allocator, float *in, float *out, int *num_out, float threshold, int num_items) {
@@ -71,5 +86,60 @@ void CountNonZero(cub::CachingDeviceAllocator &g_allocator, float *in, float *ou
     CubDebugExit(cub::DeviceSelect::If(d_temp_storage, temp_storage_bytes, in, out, num_out, num_items, select_op));
     CubDebugExit(g_allocator.DeviceAllocate(&d_temp_storage, temp_storage_bytes));
     CubDebugExit(cub::DeviceSelect::If(d_temp_storage, temp_storage_bytes, in, out, num_out, num_items, select_op));
+    if (d_temp_storage) CubDebugExit(g_allocator.DeviceFree(d_temp_storage));
+}
+
+__global__ void GetKV(float2 *in, int *keys, float *vals, int num_items) {
+    size_t STRIDE = gridDim.x * blockDim.x;
+    for (int i = blockDim.x * blockIdx.x + threadIdx.x; i < num_items; i+=STRIDE) {
+        keys[i] = in[i].x;
+        vals[i] = in[i].y;
+    }
+}
+
+__global__ void CreateKV(int *keys, float *vals, float2 *out, int num_items) {
+    size_t STRIDE = gridDim.x * blockDim.x;
+    for (int i = blockDim.x * blockIdx.x + threadIdx.x; i < num_items; i+=STRIDE) {
+        float2 kv_pair = make_float2(keys[i], vals[i]);
+        out[i] = kv_pair;
+    }
+}
+
+template<int NUM_THREADS>
+void GetFiltered(cub::CachingDeviceAllocator &g_allocator, float *in_vals, int *in_keys, float *out_vals, int *out_keys, int *num_out, float threshold, int num_items) {
+    float2 *input_kvs = NULL;
+    float2 *output_kvs = NULL;
+    CubDebugExit(g_allocator.DeviceAllocate((void**)&input_kvs, sizeof(float2)*num_items));
+    CubDebugExit(g_allocator.DeviceAllocate((void**)&output_kvs, sizeof(float2)*num_items));
+    CreateKV<<<8, NUM_THREADS>>>(in_keys, in_vals, input_kvs, num_items);
+
+    LargerThanKV select_op(threshold);
+    void *d_temp_storage = NULL;
+    size_t temp_storage_bytes = 0;
+
+    CubDebugExit(cub::DeviceSelect::If(d_temp_storage, temp_storage_bytes, input_kvs, output_kvs, num_out, num_items, select_op));
+    CubDebugExit(g_allocator.DeviceAllocate(&d_temp_storage, temp_storage_bytes));
+    CubDebugExit(cub::DeviceSelect::If(d_temp_storage, temp_storage_bytes, input_kvs, output_kvs, num_out, num_items, select_op));
+
+    GetKV<<<8, NUM_THREADS>>>(output_kvs, out_keys, out_vals, num_items);
+
+    if (input_kvs) CubDebugExit(g_allocator.DeviceFree(input_kvs));
+    if (output_kvs) CubDebugExit(g_allocator.DeviceFree(output_kvs));
+    if (d_temp_storage) CubDebugExit(g_allocator.DeviceFree(d_temp_storage));
+}
+
+void TrimmedTopK(cub::CachingDeviceAllocator &g_allocator, float *in, float *out, int *num_out, int k, int num_items) {
+    // compute mean and max
+    // epsilon = 0.2
+    // ratio = 1-epsilon
+    // count non-zero abs(X) > threshold
+    // while nnz > k
+    //  threshold = mean + ratio * (max - mean)
+    //  nnz = count nnz
+    //  ratio = ratio - epsilon
+    // end while
+    // indices = get indices
+    // values = get values
+    return;
 }
 
--- main.cu
+++ main.cu
@@ -6,13 +6,18 @@
 void test(cub::CachingDeviceAllocator &g_allocator,
           float* in,
           float* d_in,
+          int* d_in_indices,
           float* out,
           int* out_num,
+          int* out_indices,
           float* d_out,
           int* d_out_num,
+          int* d_out_indices,
           float* out_mean_ref,
           float* out_max_ref,
           int* out_num_ref,
+          int* out_indices_ref,
+          float* out_ref,
           float threshold,
           int num_elements)
 { 
@@ -28,7 +33,7 @@ void test(cub::CachingDeviceAllocator &g_allocator,
         printf("mean kernel result is wrong: %f %f\n", out[0], out_mean_ref[0]);
     }
     if (flag)
-        printf("mean kernel passed.\n");
+        printf("mean kernel passed. %f %f\n", out[0], out_mean_ref[0]);
 
     MaxKernel(g_allocator, d_in, d_out, num_elements);
     CubDebugExit(cudaMemcpy(out, d_out, 1*sizeof(float), cudaMemcpyDeviceToHost));
@@ -40,9 +45,10 @@ void test(cub::CachingDeviceAllocator &g_allocator,
         printf("max kernel result is wrong: %f %f\n", out[0], out_max_ref[0]);
     }
     if (flag)
-        printf("max kernel passed.\n");
+        printf("max kernel passed. %f %f\n", out[0], out_max_ref[0]);
 
     CountNonZero(g_allocator, d_in, d_out, d_out_num, threshold, num_elements);
+    CubDebugExit(cudaDeviceSynchronize());
     CubDebugExit(cudaMemcpy(out, d_out, 1*sizeof(float), cudaMemcpyDeviceToHost));
     CubDebugExit(cudaMemcpy(out_num, d_out_num, 1*sizeof(int), cudaMemcpyDeviceToHost));
 
@@ -53,7 +59,37 @@ void test(cub::CachingDeviceAllocator &g_allocator,
         printf("count nnz kernel result is wrong: %d %d\n", out_num[0], out_num_ref[0]);
     }
     if (flag)
-        printf("max kernel passed.\n");
+        printf("count nnz kernel passed. out_num: %d out_num_ref: %d\n", out_num[0], out_num_ref[0]);
+
+    CubDebugExit(cudaMemcpy(d_in_indices, out_indices, num_elements*sizeof(int), cudaMemcpyHostToDevice));
+
+    GetFiltered<1024>(g_allocator, d_in, d_in_indices, d_out, d_out_indices, d_out_num, threshold, num_elements);
+    CubDebugExit(cudaMemcpy(out_num, d_out_num, 1*sizeof(int), cudaMemcpyDeviceToHost));
+    CubDebugExit(cudaMemcpy(out, d_out, out_num[0]*sizeof(float), cudaMemcpyDeviceToHost));
+    CubDebugExit(cudaMemcpy(out_indices, d_out_indices, out_num[0]*sizeof(int), cudaMemcpyDeviceToHost));
+
+    flag = true;
+    if (abs(out_num[0] - out_num_ref[0]) > 0.001f)
+    {
+        flag = false;
+        printf("get filtered kernel result is wrong: %d %d\n", out_num[0], out_num_ref[0]);
+    }
+
+    for (int i = 0; i < out_num[0]; ++i) {
+        if (abs(out[i] - out_ref[i]) > 0.001f) {
+            flag = false;
+            printf("get filtered kernel result is wrong at idx %d: %f %f", i, out[i], out_ref[i]);
+            break;
+        }
+        if (out_indices[i] != out_indices_ref[i]) {
+            flag = false;
+            printf("get filtered kernel indices result is wrong at idx %d: %d %d", i, out_indices[i], out_indices_ref[i]);
+            break;
+        }
+    }
+    if (flag)
+        printf("get filtered kernel passed.\n");
+
 }
 
 int main()
@@ -62,29 +98,40 @@ int main()
     int num_elements = 512*256*24;
     
     float* data = (float*)malloc(num_elements * sizeof(float));
-    float* result = (float*)malloc(1 * sizeof(float));
+    float* result = (float*)malloc(num_elements * sizeof(float));
+    int* result_indices = (int*)malloc(num_elements * sizeof(int));
     int* result_num = (int*)malloc(1 * sizeof(int));
     float* result_ref_max = (float*)malloc(1 * sizeof(float));
     float* result_ref_mean = (float*)malloc(1 * sizeof(float));
+    float* result_ref_vals = (float*)malloc(num_elements * sizeof(float));
+    int* result_ref_indices = (int*)malloc(num_elements * sizeof(int));
     int* result_ref_num = (int*)malloc(1 * sizeof(int));
     
-    memset(result_ref_max, -1.0f, 1 * sizeof(float));
+    memset(result_ref_max, 0.0f, 1 * sizeof(float));
     memset(result_ref_mean, 0.0f, 1 * sizeof(float));
+    memset(result_ref_vals, 0.0f, num_elements * sizeof(float));
     memset(result, 0.0f, 1 * sizeof(float));
     memset(result_ref_num, 0, 1 * sizeof(int));
     memset(result_num, 0, 1 * sizeof(int));
+    memset(result_indices, 0, num_elements * sizeof(int));
+    memset(result_ref_indices, 0, num_elements * sizeof(int));
 
     float* d_data = NULL;
+    int* d_data_indices = NULL;
     float* d_result = NULL;
     int* d_result_num = NULL;
+    int* d_result_indices = NULL;
     float threshold = 5.0f;
 
     cub::CachingDeviceAllocator g_allocator(true);
 
     CubDebugExit(g_allocator.DeviceAllocate((void**)&d_data, sizeof(float)*num_elements));
+    CubDebugExit(g_allocator.DeviceAllocate((void**)&d_data_indices, sizeof(int)*num_elements));
     CubDebugExit(g_allocator.DeviceAllocate((void**)&d_result, sizeof(float)*num_elements));
     CubDebugExit(g_allocator.DeviceAllocate((void**)&d_result_num, sizeof(int)*1));
+    CubDebugExit(g_allocator.DeviceAllocate((void**)&d_result_indices, sizeof(int)*num_elements));
 
+    int acc = 0;
     for (int i = 0; i < num_elements; ++i)
     {
         data[i] = rand()%10*1.0f;
@@ -95,8 +142,11 @@ int main()
         if (result_ref_max[0] < data[i]) {
             result_ref_max[0] = data[i];
         }
+        result_indices[i] = i;
         if (data[i] > threshold) {
+            result_ref_indices[acc] = i;
             result_ref_num[0] += 1;
+            result_ref_vals[acc++] = data[i];
         }
     }
     result_ref_mean[0] = result_ref_mean[0] / num_elements;
@@ -107,23 +157,33 @@ int main()
     test(g_allocator,
           data,
           d_data,
+          d_data_indices,
           result,
           result_num,
-          d_data,
+          result_indices,
+          d_result,
           d_result_num,
+          d_result_indices,
           result_ref_mean,
           result_ref_max,
           result_ref_num,
+          result_ref_indices,
+          result_ref_vals,
           threshold,
           num_elements);
 
     if (data) delete[] data;
     if (result) delete[] result;
+    if (result_indices) delete[] result_indices;
     if (result_ref_max) delete[] result_ref_max;
     if (result_ref_mean) delete[] result_ref_mean;
+    if (result_ref_vals) delete[] result_ref_vals;
     if (result_num) delete[] result_num;
     if (result_ref_num) delete[] result_ref_num;
+    if (result_ref_indices) delete[] result_ref_indices;
     if (d_data) cudaFree(d_data);
+    if (d_data_indices) cudaFree(d_data_indices);
     if (d_result) cudaFree(d_result);
     if (d_result_num) cudaFree(d_result_num);
+    if (d_result_indices) cudaFree(d_result_indices);
 }
